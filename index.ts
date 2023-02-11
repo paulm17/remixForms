63390f7d280f318a74d42837876818ece5263791
Paul Moss <paulmoss06@gmail.com>
2023-02-11 17:49:57 +0000

    remove mantine

--- index.ts
+++ index.ts
@@ -1,12 +1,6 @@
 export { Form } from "./createForm"
 export { useField } from "./createField"
 
-export {
-  MantineButton as FormButton,
-  MantineInput as FormInput,
-  MantineSelect as FormSelect,
-} from "./mantineComponent"
-
 export type {
   FormProps,
   RenderFieldProps,
